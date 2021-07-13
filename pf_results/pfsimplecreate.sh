fc33fec4cbd61044c6688711e98d91a5e9a12d81
julnow <jul.nowk@gmail.com>
2021-07-13 09:28:51 +0200

    bash macros for pfsimple

---
+++ pf_results/pfsimplecreate.sh
@@ -0,0 +1,11 @@
+#!/bin/#!/usr/bin/env bash
+
+#create inputfile from pfsimple
+if [[ $# -eq 0 ]] ; then
+    echo 'no name passed'
+    exit 1
+else
+  mkdir ~/pf_results/lambda/$1
+  cd ~/pf_results/lambda/$1
+  ~/pf_simple/install/bin/main ~/fileslist.txt
+fi
