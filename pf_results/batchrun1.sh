018a298e70d5988225600eebdaeb5857b9876d20
julnow <jul.nowk@gmail.com>
2021-07-16 11:00:53 +0200

    batch scripts for pfsimple

--- pf_results/batchrun1.sh
+++ pf_results/batchrun1.sh
@@ -4,10 +4,10 @@ mkdir -p $LOGDIR
 mkdir -p $LOGDIR/out
 mkdir -p $LOGDIR/error
 
-sbatch --job-name=pfsimplecreate \
+sbatch --job-name=pfsimpleread \
         -t 00:20:00 \
         --partition main \
         --output=$LOGDIR/out/%j.out.log \
         --error=$LOGDIR/error/%j.err.log \
         --array 1-4 \
-        -- $PWD/slurm_pfsimplecreate.sh
+        -- $PWD/slurm_pfsimpleread.sh
--- pf_results/slurm_pfsimplecreate.sh
+++ pf_results/slurm_pfsimplecreate.sh
@@ -3,10 +3,11 @@
 #remote work on AnalysisTree
 source /lustre/cbm/users/lubynets/soft/root-6/install-cpp11/bin/thisroot.sh
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lustre/cbm/users/$USER/AnalysisTree/install/lib/
-export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:/lustre/cbm/users/$USER/AnalysisTree/install/include/AnalysisTree/
+export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:/lustre/cbm/users/$USER/AnalysisTree/install/include/AnalysisTree
 #for Pf_simple
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lustre/cbm/users/$USER/pf_simple/install/external/lib/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lustre/cbm/users/$USER/pf_simple/install/lib/
+export AnalysisTree_DIR=/lustre/cbm/users/$USER/AnalysisTree/install/lib/cmake/AnalysisTree
 
 INDEX=${SLURM_ARRAY_TASK_ID}
 WORK=/lustre/cbm/users/$USER
