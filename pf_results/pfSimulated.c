22e6edd44b6a0fd4ecee2908d74777bd16789207
julnow <jul.nowk@gmail.com>
2021-07-16 10:58:27 +0200

    new axis names

--- pf_results/pfSimulated.c
+++ pf_results/pfSimulated.c
@@ -23,9 +23,9 @@ TH1F hsimspz("hsimspz", "#Lambda_{sim} pz;pz [GeV/c];dN/dpz", 100, 0, 14);
 TH1F hsimspt("hsimspt", "#Lambda_{sim} pt;pt [GeV/c];dN/dpt", 100, -1, 3);
 TH1F hsimsp("hsimsp", "#Lambda_{sim} p;p [GeV/c];dN/dp", 100, 0, 14);
 //correlations for sim
-TH2F hcors_rap_pt("hcors_rap_pt", "correlation #Lambda_{sim} rapidity pt; rapidity; pT", 100, -1, 4,  100, -1, 4);
-TH2F hcors_phi_pt("hcors_phi_pt", "correlation #Lambda_{sim} #phi pt; #phi; pT", 100, -5, 5,  100, -0.01, 5);
-TH2F hcors_px_py("hcors_px_py", "correlations #Lambda_{sim} px py; px; py", 100, -3, 3,  100, -3, 3);
+TH2F hcors_rap_pt("hcors_rap_pt", "correlation #Lambda_{sim} rapidity pt; rapidity; pT [GeV/c]", 100, -1, 4,  100, -1, 4);
+TH2F hcors_phi_pt("hcors_phi_pt", "correlation #Lambda_{sim} #phi pt; #phi [#circ]; pT [GeV/c]", 100, -5, 5,  100, -0.01, 5);
+TH2F hcors_px_py("hcors_px_py", "correlations #Lambda_{sim} px py; px [GeV/c]; py [GeV/c]", 100, -3, 3,  100, -3, 3);
 
 //reading of data
 
