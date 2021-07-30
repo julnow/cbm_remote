6fdd077dc0b47175002005a4a5dd54b3561b1b5d
julnow <jul.nowk@gmail.com>
2021-07-30 15:34:31 +0200

    preparing files for ml

---
+++ ml/batchrun_create_sign.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+LOGDIR=/lustre/cbm/users/$USER/ml/log
+mkdir -p $LOGDIR
+mkdir -p $LOGDIR/out
+mkdir -p $LOGDIR/error
+
+sbatch --job-name=pfsimplecreate \
+        -t 00:20:00 \
+        --partition main \
+        --output=$LOGDIR/out/%j.out.log \
+        --error=$LOGDIR/error/%j.err.log \
+        --array 1-500 \
+        -- $PWD/slurm_pfsimplecreate_sign.sh
---
+++ ml/slurm_pfsimplecreate_sign.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#remote work on AnalysisTree
+source /lustre/cbm/users/lubynets/soft/root-6/install-cpp11/bin/thisroot.sh
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lustre/cbm/users/$USER/AnalysisTree/install/lib/
+export ROOT_INCLUDE_PATH=$ROOT_INCLUDE_PATH:/lustre/cbm/users/$USER/AnalysisTree/install/include/AnalysisTree
+#for Pf_simple
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lustre/cbm/users/$USER/pf_simple/install/external/lib/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/lustre/cbm/users/$USER/pf_simple/install/lib/
+export AnalysisTree_DIR=/lustre/cbm/users/$USER/AnalysisTree/install/lib/cmake/AnalysisTree
+
+INDEX=${SLURM_ARRAY_TASK_ID}
+WORK=/lustre/cbm/users/$USER
+#path to pf simpled
+pfpath=$WORK/pf_simple/main_sign
+#path to simulated files
+
+#splitting into 500 fileslist with 10 files
+mkdir -p $PWD/fileslists
+list=$PWD/fileslists/filelist_${INDEX}.txt
+: >  $list
+for ((i=1+10*(INDEX-1); i<=10*INDEX; i++)); do
+  echo /lustre/cbm/users/lubynets/cbm2atree/outputs/apr20_fr_18.2.1_fs_jun19p1/dcmqgsm_smm_pluto/auau/12agev/mbias/sis100_electron_target_25_mkm/AT2/$i/$i.analysistree.root >>  $list
+done
+mkdir -p $PWD/pfsimple/${INDEX}/sign
+cd $PWD/pfsimple/${INDEX}/sign
+#run pf_simple on each file
+$pfpath $WORK/ml/fileslists/filelist_${INDEX}.txt
